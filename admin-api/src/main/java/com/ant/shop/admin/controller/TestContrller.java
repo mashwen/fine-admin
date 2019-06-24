ea2504e4ac03e3cdfffb6726f70a1a641f1dac46
Venky <venkychu@qq.com>
2019-06-24 20:07:50 +0800

    update auth

--- admin-api/src/main/java/com/ant/shop/admin/controller/TestContrller.java
+++ admin-api/src/main/java/com/ant/shop/admin/controller/TestContrller.java
@@ -1,30 +1,30 @@
 package com.ant.shop.admin.controller;
 
 
-import com.alibaba.fastjson.JSON;
+import com.ant.shop.asorm.entity.FineStaff;
+import com.ant.shop.asorm.mapper.FineStaffMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.security.Principal;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api")
 public class TestContrller {
 
 
-
+    @Autowired
+    private FineStaffMapper fineStaffMapper;
 
     @GetMapping("/test")
     @ResponseBody
-    public  String test(HttpServletRequest request) {
-
-        return "111111";
+    public FineStaff test(Principal member) {
+        Integer userId = Integer.valueOf(member.getName());
+        FineStaff fineStaff = fineStaffMapper.selectByPrimaryKey(userId);
+        System.out.println(fineStaff);
+        return fineStaff;
     }
-
 }
